489450749d660a309ea1963849cf38f4f58373ba
dmacuacua4-star <dmacuacua4@estudantes.unisced.edu.mz>
2026-09-20 18:29:28 +0200

    Update Utilizador.java

--- src/Utilizador.java
+++ src/Utilizador.java
@@ -1,1 +1,34 @@
+public class Utilizador {
 
+    private int id;
+    private String nome;
+    private String contacto;
+
+    public Utilizador(int id, String nome, String contacto) {
+        this.id = id;
+        this.nome = nome;
+        this.contacto = contacto;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getContacto() {
+        return contacto;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+            "ID: %d | Nome: %s | Contacto: %s",
+            id,
+            nome,
+            contacto
+        );
+    }
+}
